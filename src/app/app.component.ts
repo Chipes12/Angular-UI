26660b82642ba8403945f3c14d06567105652c18
CarlosA <is727223@iteso.mx>
2022-03-23 07:22:09 -0600

    new pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularClase';
+  title: String = 'FrontEndClase09';
+  name: String = 'Carlos';
+  flag: Boolean = false;
+
+  movies: string[] = [
+    'Los tres Huastecos',
+    'Alien',
+    'Batman',
+    'Una pelicula de huevos',
+    'Pineapple express'
+  ];
+  searchValue: string = '';
+  searchTimeOut: any;
+  filteredMovies: string[] = this.movies;
+  constructor(){
+    setTimeout(() => {
+      this.name = 'Andrés';
+      this.movies.push('Los 3 Garcia');
+    }, 2000);
+  }
+
+  ngOnInit(): void{ }
+
+  doOnClick(e: any){
+    console.log('Clikeado');
+    e.preventDefault();
+    //this.flag = !this.flag;
+    this.movies.push(this.searchValue);
+  }
+
+  doSearch(){
+    if(this.searchTimeOut) clearTimeout(this.searchTimeOut);
+
+    this.searchTimeOut = setTimeout(() => {
+      const searchValue = this.searchValue.toLowerCase();
+      this.filteredMovies = this.movies.filter(item => {
+        return item.toLowerCase().includes(searchValue);
+      });
+    }, 200);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
