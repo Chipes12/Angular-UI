2e6c4f6ce93fbc2223c36162616fc7fca73fe522
CarlosA <carlos.andres_paez@outlook.com>
2022-04-01 23:30:49 -0600

    Estilos

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
-import {LoginService} from '../../shared/services/login.service'
+import {LoginService} from '../../shared/services/login.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,10 +13,17 @@ export class LoginComponent implements OnInit {
   
   credentials: any = {};
   error: boolean = false;
+  form: FormGroup;
 
   constructor(private loginService: LoginService,
                       private authService: AuthService,
-                      private router: Router) { }
+                      private router: Router,
+                      private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      correo:['', Validators.required],
+      password:['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
